7f1fa588954ae028084fee30e0a834e7434fec17
darleg <darleg@outlook.com>
2024-01-30 08:39:19 -0800

    leave code only

---
+++ VS2022/Console/VBCon/HiThere/VBConHithere.vb
@@ -0,0 +1,8 @@
+
+Module VBConHithere
+    Sub Main(args As String())
+        Console.WriteLine("This app uses .Net 7")
+        Console.WriteLine("Hi there from Appa!")
+        Console.WriteLine("with .Net 7")
+    End Sub
+End Module
